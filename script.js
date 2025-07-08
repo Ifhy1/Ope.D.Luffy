980a948f9d11e1a9a8986d122e3b15f33949167c
Ihedioha Ifeyinwa <ihediohaifeyinwa@gmail.com>
2025-07-08 22:56:24 +0100

    feat: first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const intro = document.getElementById('intro');
+const slideshow = document.getElementById('slideshow');
+
+function startSlideshow() {
+  intro.classList.add('hidden');
+  slideshow.classList.remove('hidden');
+  slides[0].classList.add('active');
+  setInterval(showNextSlide, 4000); // change every 4 seconds
+}
+
+function showNextSlide() {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add('active');
+}
